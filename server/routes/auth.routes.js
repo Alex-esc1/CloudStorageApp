793eed94ae71bd40211eb4d15f7f6900b259b0f8
Alexander Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-04-14 15:46:38 +0300

    [Backend]  Хеширование пароля пользователя
    Мы не можем сохранять пароль в исходном виде в БД с целью безопасности пароль будет захеширован. (npm i bcryptjs - модуля для хеширования)

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs")
 const router = new Router()
 
-router.post('/registration', (req, res) => {
+router.post('/registration', async (req, res) => {
     try {
 
         const {email, password} = req.body
@@ -13,7 +14,10 @@ router.post('/registration', (req, res) => {
             return res.status(400).json({message: `Пользователь с таким email: ${email} уже существует!`})
         }
 
-        const user = new User ({email})
+        const hashPassword = await bcrypt.hash(password, 15)
+        const user = new User ({email, password: hashPassword})
+        await user.save()
+        return res.json({message: "Был создан пользователь"})
 
     } catch (e) {
         console.log(e)
