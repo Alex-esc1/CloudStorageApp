a26c8422fd00cd93df1ff0efac0567fab5c1a62f
Alexander Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-04-14 16:07:36 +0300

    [Backend] Валидация email
    Установка модуля npm i express-validator

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,20 @@
 const Router = require("express");
 const User = require("../models/User");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
+const {check, validationResult} = require("express-validator")
 const router = new Router()
 
-router.post('/registration', async (req, res) => {
+router.post('/registration', 
+    [
+        check('email', "Некорректный email").isEmail(),
+        check('password', "Пароль должен быть больше 3-х символов и не более 12-ти").isLength({min:3, max:12})
+    ],
+    async (req, res) => {
     try {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            return res.status(400).json({message: "Неверный запрос", errors})
+        }
 
         const {email, password} = req.body
 
@@ -26,3 +36,5 @@ router.post('/registration', async (req, res) => {
     }
 
 })
+
+module.exports = router
