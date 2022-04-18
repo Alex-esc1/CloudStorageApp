6e7e33906b99ce1554102b3e72e31c3a277cac3c
Alex-esc1 <85848281+Alex-esc1@users.noreply.github.com>
2022-04-18 16:59:10 +0300

    [Frontend] Input useState
    Делаю инпуты управляемыми

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../utils/input/Input';
 import "./registration.css"
 
 const Registration = () => {
+    const [email, setEmail] = useState ("")
+    const [password, setPassword] = useState ("")
     return (
         <div className='registration'>
             <div className="registration__header">Регистрация</div>
-            <Input type="text" placeholder="Введите email..."/>
-            <Input type="password" placeholder="Введите пароль..."/>
+            <Input value={email} setValue={setEmail} type="text" placeholder="Введите email..."/>
+            <Input value={password} setValue={setPassword} type="password" placeholder="Введите пароль..."/>
             <button className="registration_btn">Войти</button>
         </div>
     );
