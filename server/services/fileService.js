059b7ad5ff78290430c9cebf722d6388d45ec448
Alexander Shilenkov <alexesc154@gmail.com>
2022-04-21 15:44:56 +0300

    [Backend] Правка ошибки создания файла
    Неверное передал наименование  в функцию создания файла

--- server/services/fileService.js
+++ server/services/fileService.js
@@ -4,7 +4,7 @@ const config = require('config');
 
 class FileService {
 
-    createDir(File) {
+    createDir(file) {
         const filePath = `${config.get('filePath')}\\${file.user}\\${file.path}`
         return new Promise(((resolve, reject) => {
             try {
