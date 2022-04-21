e169e4ad7324c66af6472928028b14955e1bece9
Alexander Shilenkov <alexesc154@gmail.com>
2022-04-21 15:17:21 +0300

    [Backend] files хранение
    Хранить файлы буду локально без подключения стороннего сервиса, для этого прописываю путь к директории files и реализую логику

--- server/services/fileService.js
+++ server/services/fileService.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
 const File = require('../models/File');
+const config = require('config');
 
 class FileService {
 
     createDir(File) {
+        const filePath = `${config.get('filePath')}\\${file.user}\\${file.path}`
         return new Promise(((resolve, reject) => {
             try {
-
+                if(!fs.existsSync(filePath)) {
+                    fs.mkdirSync(filePath)
+                    return resolve ({message: "Файл был создан"})
+                } else {
+                    return reject ({message: "Файл уже существует"})
+                }
             } catch (e) {
                 return reject ({message: "Ошибка файла!"})
             }
