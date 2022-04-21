250e496bf99a940104d5d92b146ac98b46795bb5
Alexander Shilenkov <alexesc154@gmail.com>
2022-04-21 15:40:48 +0300

    [Backend] file controller
    Работа с запросами, создание локальной папки под каждого пользователя. Получение файла

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,6 +6,8 @@ const jwt = require("jsonwebtoken")
 const {check, validationResult} = require("express-validator")
 const router = new Router()
 const authMiddleware = require('../middleware/auth.middleware')
+const fileService = require('../services/fileService')
+const File = require('../models/File')
 
 router.post('/registration',
     [
@@ -13,25 +15,26 @@ router.post('/registration',
         check('password', 'Password must be longer than 3 and shorter than 12').isLength({min:3, max:12})
     ],
     async (req, res) => {
-    try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.status(400).json({message: "Uncorrect request", errors})
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message: "Uncorrect request", errors})
+            }
+            const {email, password} = req.body
+            const candidate = await User.findOne({email})
+            if(candidate) {
+                return res.status(400).json({message: `User with email ${email} already exist`})
+            }
+            const hashPassword = await bcrypt.hash(password, 8)
+            const user = new User({email, password: hashPassword})
+            await user.save()
+            await fileService.createDir(new File({user:user.id, name: ''}))
+            res.json({message: "User was created"})
+        } catch (e) {
+            console.log(e)
+            res.send({message: "Server error"})
         }
-        const {email, password} = req.body
-        const candidate = await User.findOne({email})
-        if(candidate) {
-            return res.status(400).json({message: `User with email ${email} already exist`})
-        }
-        const hashPassword = await bcrypt.hash(password, 8)
-        const user = new User({email, password: hashPassword})
-        await user.save()
-        res.json({message: "User was created"})
-    } catch (e) {
-        console.log(e)
-        res.send({message: "Server error"})
-    }
-})
+    })
 
 
 router.post('/login',
