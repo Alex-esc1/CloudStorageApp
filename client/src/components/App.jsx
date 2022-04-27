cf9cfd3b9ddc0423e1cd31c9e91f058b32ffbcee
Alexander Shilenkov <alexesc154@gmail.com>
2022-04-27 17:19:35 +0300

    [Frontend] Route для компонента disk
    Прописываю роут компонента диск в App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react';
 import Navbar from "./navbar/Navbar";
 import './app.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import Registration from "./authorization/Registration";
 import Login from "./authorization/Login";
 import {useDispatch, useSelector} from "react-redux";
 import {auth} from "../actions/user";
+import Disk from "./disk/Disk";
 
 function App() {
     const isAuth = useSelector(state => state.user.isAuth)
@@ -15,16 +16,21 @@ function App() {
         dispatch(auth())
     }, [])
 
-
     return (
         <BrowserRouter>
             <div className='app'>
                 <Navbar/>
                 <div className="wrap">
-                    {!isAuth &&
-                      <Routes>
+                    {!isAuth ?
+                    <Routes>
                       <Route path="/registration" element={<Registration/>}></Route>
                       <Route path="/login" element={<Login/>}/>
+                      <Route path="*" element={<Navigate to="login" replace />} />
+                    </Routes>
+                    :
+                    <Routes>
+                        <Route exact path="/" element={<Disk/>}/>
+                        <Route path="*" element={<Navigate to="/" replace />} />
                     </Routes>
                     }
                 </div>
